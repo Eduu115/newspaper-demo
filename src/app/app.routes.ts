2abfa5147ed764bbb0ed347f6b3aa7738df7acd5
Tony1406 <anthony.go0609@gmail.com>
2026-02-02 23:40:58 +0100

    routing, borrador de vistas, y estructura de archivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './vistas/home/home';
+import { Login } from './vistas/login/login';
+import { DetallesNoticia } from './vistas/detalles-noticia/detalles-noticia';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'noticia/:id', component: DetallesNoticia },
+    { path: 'login', component: Login },
+    { path: '**', redirectTo: '' }
+];
