71302fb6c81ba3ec0cdd6174ec9371d27b6be5a7
Juan Guevara <manzanillajuan41@gmail.com>
2026-02-05 13:07:10 +0100

    minor modifications

--- src/app/components/nav/nav.ts
+++ src/app/components/nav/nav.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
